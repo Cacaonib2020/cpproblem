734404cf3757c7d2a9f6269bdc70a09684885b5f
Cacaonib2020 <91684527+Cacaonib2020@users.noreply.github.com>
2022-05-18 23:28:51 +0900

    int to long long

--- Answered/D.cpp
+++ Answered/D.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
-int n, ans = 0;
+long long n, ans = 0;
 
 int main() {
 	cin >> n;
-	for (int i = 1; i <= n; i++)
+	for (long long i = 1; i <= n; i++)
 	{
 		ans += i;
 	}
